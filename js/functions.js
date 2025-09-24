36c89d84cbc8db125cf4a406b153ed635c7834b3
Dimriu <dimriudobrunin@gmail.com>
2025-09-24 13:45:30 +0300

    исправил ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 // Функция для проверки длины строки
-let checkLengthString = function (string, length) {
+const checkLengthString = function (string, length) {
   if(string.length <= length) {
     return true;
   }
@@ -7,39 +7,41 @@ let checkLengthString = function (string, length) {
 };
 //checkLengthString('проверяемая строка', 10);
 
+
 // Функция для проверки, является ли строка палиндромом
-let isPalindrome = function (string) {
+const isPalindrome = function (string) {
   string = string.replaceAll(' ','');
   string = string.toLowerCase();
   let stringNew = '';
-  for (let i=string.length-1; i>=0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     stringNew += string[i];
-    }
+  }
   if(string === stringNew) {
     return true;
-    }
+  }
   return false;
 };
 //isPalindrome('Лёша на полке клопа нашёл ');
 
 
-let returnNumber = function (string) {
+const returnNumber = function (string) {
   string = string.toString();
 
-  let arrayIndexs = [];
+  const arrayIndexs = [];
   let count = 1;
-  for (let i=0; i<string.length; i++) {
-    if (string[i] === " ") {
-        count = count + 1;
-        arrayIndexs.push(i);
-      }
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      count = count + 1;
+      arrayIndexs.push(i);
     }
+  }
   arrayIndexs.push(string.length);
 
-  let arrayValue = [];
-  for (let j=0; j<count; j++) {
-    let elementString ='';
-    for (j===0 ? k=0 : k=(arrayIndexs[j-1]); k<arrayIndexs[j]; k++) {
+  const arrayValue = [];
+  for (let j = 0; j < count; j++) {
+    let elementString = '';
+    let k;
+    for (j === 0 ?  k = 0 : k = (arrayIndexs[j - 1]); k < arrayIndexs[j]; k++) {
       elementString = elementString + string[k];
     }
     arrayValue.push(elementString);
@@ -47,19 +49,19 @@ let returnNumber = function (string) {
 
 
   let message = '';
-  for (let j=0; j<arrayValue.length; j++) {
-    let temp = (arrayValue[j].replaceAll(' ', ''));
-      for (let i=0; i<temp.length; i++) {
-        if (!Number.isNaN(parseInt(temp[i]))) {
-          message+=temp[i];
+  for (let j = 0; j < arrayValue.length; j++) {
+    const temp = (arrayValue[j].replaceAll(' ', ''));
+    for (let i = 0; i < temp.length; i++) {
+      if (!Number.isNaN(parseInt(temp[i]))) {
+        message += temp[i];
       }
     }
   }
 
 
   if(message.length > 0) {
-      return (Number(message));
-    }
+    return (Number(message));
+  }
   return NaN;
 
 };
