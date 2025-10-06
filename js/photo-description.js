4198b1997643e098f7895eb9c1e74b901b8a164c
Dimriu <dimriudobrunin@gmail.com>
2025-10-06 08:24:06 +0300

    Д/з Модуляция

---
+++ js/photo-description.js
@@ -0,0 +1,23 @@
+import { getInitialData } from './data.js';
+import { getCommentDescription } from './comments-description.js';
+import { getRandomInteger, getRandomArrayElement, createIdGenerator } from './utils.js';
+
+const [INITIAL_DATA, DESCRIPTIONS_PHOTOS] = getInitialData();
+const generatePhotoId = createIdGenerator();
+const generatePhotoNumber = createIdGenerator();
+
+const generatePhotoDescription = function () {
+  return {
+    id: generatePhotoId(),
+    url: `photos/${generatePhotoNumber()}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS_PHOTOS),
+    likes: getRandomInteger(INITIAL_DATA.MIN_LIKES, INITIAL_DATA.MAX_LIKES),
+    comments: getCommentDescription()
+  };
+};
+
+const getPhotoDescription = function () {
+  return Array.from({length: INITIAL_DATA.COUNT_GENERATED_OBJECTS}, generatePhotoDescription);
+};
+
+export { getPhotoDescription };
